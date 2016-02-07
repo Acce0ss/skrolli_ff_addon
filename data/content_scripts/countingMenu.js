c3658306da2b625e94dd65c82c2fa8a4efb727ca
Asser Lähdemäki <asser@lahdemaki.fi>
2016-02-08 00:46:54 +0200

    Add basic examples of adding a button, attaching a panel to that button, defining the panel design in HTML, communicating with a content script, interacting with page DOM via a content script, automatically injecting a content script using PageMod

---
+++ data/content_scripts/countingMenu.js
@@ -0,0 +1,20 @@
+var wordToCount = document.getElementById("keyword");
+var pageModCheckbox = document.getElementById("enable");
+var countButton = document.getElementById("countButton");
+
+countButton.disabled = true;
+
+pageModCheckbox.addEventListener('change', function(){
+    self.port.emit('pagemod-enabled-changed', pageModCheckbox.checked);
+});
+
+wordToCount.addEventListener('input', function(){
+    
+    countButton.disabled = (wordToCount.value.length <= 0);
+    
+});
+
+countButton.addEventListener('click', function(){
+
+    self.port.emit('count-requested', wordToCount.value);
+});
--- index.js
+++ index.js
@@ -1,9 +1,71 @@
 var self = require("sdk/self");
 
-// a dummy function, to show how tests work.
-// to see how to test this function, look at test/test-index.js
-function dummy(text, callback) {
-  callback(text);
+var pageMod = require("sdk/page-mod");
+
+var ui = require("sdk/ui");
+var tabs = require("sdk/tabs");
+var panels = require("sdk/panel");
+
+var filepick = require("./lib/filepicker.js");
+
+var menuButton = ui.ToggleButton(
+    {
+	id: "menutoggle",
+	label: "Show counting menu",
+	icon: {
+	    "16":"./icon-16.png",
+	    "32":"./icon-32.png",
+	    "64":"./icon-64.png"
+	},
+	onChange: handleChange
+    }
+);
+
+var countingMenu = panels.Panel({
+
+    contentURL: "./content_html/countingMenu.html",
+    contentScriptFile: "./content_scripts/countingMenu.js",
+    onHide: handleHide
+});
+
+countingMenu.port.on('count-requested', function(keyword){
+    var countWorker = tabs.activeTab.attach({
+	contentScriptFile: "./content_scripts/countall.js"
+    });
+    countWorker.port.emit('count-words', keyword);
+});
+
+var skrolliPageMod = null;
+
+countingMenu.port.on('pagemod-enabled-changed', function(enabled){
+    if(enabled)
+    {
+	skrolliPageMod = pageMod.PageMod({
+	    include: '*.skrolli.fi',
+	    contentScriptFile: './content_scripts/countall.js',
+	    onAttach: function(worker){
+		worker.port.emit('count-words', '[Ss]krolli');
+	    }
+	});
+    }
+    else
+    {
+	skrolliPageMod.destroy();
+    }
+});
+
+function handleChange(state) {
+
+    if (state.checked) {
+	countingMenu.show({
+	    position: menuButton
+	});
+    }
+    
 }
 
-exports.dummy = dummy;
+function handleHide(){
+    menuButton.state('window', {checked: false});
+}
+
+
